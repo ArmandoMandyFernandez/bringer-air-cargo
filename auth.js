a3d854dd8ac67b473abd310d933862a94bdcd66e
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-09-28 22:39:35 -0400

    added handle errors to test deployed api

--- auth.js
+++ auth.js
@@ -3,16 +3,24 @@ const secretKey = process.env.SECRET_KEY;
 
 // creating the function for the generation of the JWT Token
 function generateToken(req, res) {
+    try {
+        // Validating input data
+        const { login, password } = req.body;
+        if (!login || !password) {
+            return res.status(400).json({ error: "Missing login or password" });
+        }
 
-    // Storing the variables of login and password
-    const { login, password } = req.body;
+        // Create a JWT Token
+        const token = jwt.sign({ login, password }, secretKey);
 
-    // Combining the login and password to create the JWT Token
-    const token = jwt.sign({ login, password }, secretKey);
+        // Response with the token
+        res.status(200).json({ token });
+    } catch (error) {
 
-    // creating a json with the token
-    res.json({ token });
+        // Handle error
+        res.status(500).json({ error: "Internal server error" });
+    }
 }
 
-// exporting function to use in index.js
+// exporting function to use in server.js
 module.exports = { generateToken };
