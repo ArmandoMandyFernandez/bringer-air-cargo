2c8e40f2f0a188b2656dfdebc24d75ff0f7d853d
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-09-28 22:02:13 -0400

    changed index to server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 8888;
+const PORT = 8888;
 
 app.use(express.json());
 app.use(cors());
