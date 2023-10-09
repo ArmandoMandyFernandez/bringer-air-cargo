5de7e3951006647c4abf73000082eea060793e24
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-09 16:55:24 -0400

    changing the bearer to the hard coded bearer token

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ async function trackParcel(req, res) {
             {
                 headers: {
                     // Getting the Bearer token from the .env
-                    Authorization: `Bearer ${bearer}`,
+                    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6ImFjY2VzcyJ9.eyJpYXQiOjE2NzIzMjY1NTUsImV4cCI6MTcwMzg2MjU1NSwiYXVkIjoiaHR0cHM6Ly9icmluZ2VycGFyY2VsLmNvbSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiNTI1eXM2YWh4d3UyIiwianRpIjoiZDdlZGE3NDgtNzMxOS00YWIzLWI2MGEtMDEzMzI0NmVkNmY2In0.uJi6d6-E2zDWj24wryh2sVWKs4ceny4QllbrHrzK5L0`,
                 },
             }
         );
